fb15d9843eb1e9c94bcbc2844fc0127ec7d0015b
Diogo Ferreira Fernandes <dio@Dio-Macbook.local>
2020-12-18 00:38:44 -0300

    Add analytics to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,12 @@ export default function Footer() {
   return (
     <footer className={ styles.footer }>
       <p>Criado por <strong>Diogo Ferreira Fernandes</strong></p>
+      <p>
+        <Link href="/analytics">
+          <a>Analytics</a>
+        </Link>
+      </p>
     </footer>
-  )
+  );
 }
 
