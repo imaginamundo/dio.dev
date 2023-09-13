d8281c0aa559ad787b55ce4d0bd833cbbb89f2ed
Diogo Fernandes <diogo.ff@icloud.com>
2023-09-13 00:12:54 -0300

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dio.dev'
+  site: 'https://dio.dev',
+  integrations: [sitemap()]
 });
